82885297c92c5f1ea5e47aab189d43e4d9a69f36
BubaInc <neumeier.nicolas@outlook.com>
2025-05-18 18:21:27 +0200

    jhgjghhgjg

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from manim-animations!")
+from manim import *
 
-
-if __name__ == "__main__":
-    main()
+class ThreeDCameraRotation(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        circle=Circle()
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(circle,axes)
+        self.begin_ambient_camera_rotation(rate=0.1)
+        self.wait()
+        self.stop_ambient_camera_rotation()
+        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.wait()
