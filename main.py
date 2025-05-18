092f1a55557557325fd2d831524853db7a4d1700
BubaInc <neumeier.nicolas@outlook.com>
2025-05-18 20:07:53 +0200

    gfgfdg

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from manim import *
 
 class ThreeDCameraRotation(ThreeDScene):
     def construct(self):
-        axes = ThreeDAxes()
-        circle=Circle()
-        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.add(circle,axes)
-        self.begin_ambient_camera_rotation(rate=0.1)
-        self.wait()
-        self.stop_ambient_camera_rotation()
-        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.wait()
+        axes = Axes(
+            x_range=[-3, 3],
+            y_range=[-3, 3],
+            x_length=6,
+            y_length=6,
+            axis_config={"color": BLUE},
+        )
+
+        self.add(axes)
+
+        graph = axes.plot(lambda x: np.sin(x), color=YELLOW)
+
+        self.play(Create(graph))
+
+        # Create a tangent line at a specific point
+        # move the point and the tangent line to the right
+        tangent_line = axes.get_tangent_line(graph, 1, color=RED)
+        self.play(Create(tangent_line))
+        self.play(tangent_line.animate.shift(RIGHT * 2))
